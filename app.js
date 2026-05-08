09676b21629431a03c8ad6ea5c0222d51fdef451
samettabak17-blip <samettabak17@gmail.com>
2026-05-08 22:17:09 +0400

    Update app.js

--- app.js
+++ app.js
@@ -74,9 +74,10 @@ function corporateFallback(lang) {
 //  GEMINI CALL (2.5 PRO EXP - 2026)
 // -------------------------------
 async function callGemini(prompt) {
-  const url =
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-pro-preview:generateContent?key=" +
-    process.env.GEMINI_API_KEY;
+ const url =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-pro-preview:generateContent?key=" +
+  process.env.GEMINI_API_KEY;
+
 
   const FALLBACK =
     "Size en doğru bilgiyi sunabilmem için konuyu biraz daha netleştirebilir misiniz? Böylece ihtiyacınıza en uygun yönlendirmeyi sağlayabilirim.";
